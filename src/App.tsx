b0c93e13be03becfa960a52b5902eb213aec7970
Kat10-4 <143129834+Kat10-4@users.noreply.github.com>
2024-05-05 17:47:06 +0300

    add setTasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {TaskType, ToDoList} from "./ToDoList";
+import {ToDoList} from "./ToDoList";
 
 
 function App() {
 
-    let task1:Array<TaskType> = [
+    const [tasks,setTasks]=useState([
         {id: 1, title: "CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "React", isDone: false},
-    ]
+    ])
 
-    let task2:Array<TaskType> = [
-        {id: 1, title: "Terminator", isDone: true},
-        {id: 2, title: "XXX", isDone: false},
-        {id: 3, title: "Jentlemen of Fortune", isDone: true},
-    ]
+    function removeTask(id:number){
+        let resultTasks=tasks.filter(el=>el.id!==id)
+        setTasks(resultTasks)
+    }
 
     return <div className="App">
-        <ToDoList title={"What to learn"} task={task1}/>
-        <ToDoList title={"Movies"} task={task2}/>
-        <ToDoList title={"Songs"} task={task1}/>
+        <ToDoList title={"What to learn"} task={tasks} removeTask={removeTask}/>
     </div>
 }
 
--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -9,20 +9,23 @@ export type TaskType = {
 type PropsType = {
     title: string,
     task: Array<TaskType>
+    removeTask:(id:number)=>void
 }
 
-export function ToDoList(props: PropsType) {
+export function ToDoList({title,task,removeTask}:PropsType) {
     return <div>
-        <h3>{props.title}</h3>
+        <h3>{title}</h3>
         <div>
             <input/>
             <button>+</button>
         </div>
         <ul>
-            <li><input type="checkbox" checked={props.task[0].isDone}/><span>{props.task[0].title}</span></li>
-            <li><input type="checkbox" checked={props.task[1].isDone}/><span>{props.task[1].title}</span></li>
-            <li><input type="checkbox" checked={props.task[2].isDone}/><span>{props.task[2].title}</span></li>
-
+            {task.map(t =>
+                <li key={t.id}><input type="checkbox" checked={t.isDone}/>
+                    <span>{t.title}</span>
+                    <button onClick={()=>removeTask(t.id)}>x</button>
+                </li>
+            )}
         </ul>
         <div>
             <button>All</button>
