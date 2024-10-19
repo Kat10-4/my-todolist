3574e9b704e105ee90a01b2b5a97fe339b8d1699
Kat10-4 <143129834+Kat10-4@users.noreply.github.com>
2024-10-19 22:12:24 +0300

    added tests for reducer

--- src/model/toDoListsReducer.test.ts
+++ src/model/toDoListsReducer.test.ts
@@ -1,7 +1,7 @@
 import {v1} from 'uuid';
-import {useReducer, useState} from 'react';
-import {ToDoListsType} from '../App';
+
 import {toDoListsReducer} from './toDoListsReducer';
+import {ToDoListsType} from '../App';
 
 
 test(('correct ToDoList should be removed'), () => {
@@ -13,8 +13,86 @@ test(('correct ToDoList should be removed'), () => {
         {id: toDoListId2, title: 'what to buy', filter: 'all'}
     ]
 
-    const endState = toDoListsReducer(startState, {type: 'REMOVE_TODOLIST', toDoListId1})
+    const action = {
+        type: 'REMOVE_TODOLIST',
+        payload: {
+            id: toDoListId1,
+        },
+    } as const
+
+    const endState = toDoListsReducer(startState, action)
 
     expect(endState.length).toBe(1)
     expect(endState[0].id).toBe(toDoListId2)
+})
+
+test('correct todolist should be added', () => {
+    const toDoListId1 = v1()
+    const toDoListId2 = v1()
+
+    const startState: ToDoListsType[] = [
+        {id: toDoListId1, title: 'what to learn', filter: 'all'},
+        {id: toDoListId2, title: 'what to buy', filter: 'all'}
+    ]
+
+    const action = {
+        type: 'ADD_TODOLIST',
+        payload: {
+            title: 'New Todolist',
+        },
+    } as const
+
+    const endState = toDoListsReducer(startState, action)
+
+    expect(endState.length).toBe(3)
+    expect(endState[0].title).toBe(action.payload.title)
+    expect(endState[0].filter).toBe('all')
+})
+
+test('correct todolist should change its name', () => {
+    const toDoListId1 = v1()
+    const toDoListId2 = v1()
+
+    const startState: ToDoListsType[] = [
+        {id: toDoListId1, title: 'what to learn', filter: 'all'},
+        {id: toDoListId2, title: 'what to buy', filter: 'all'}
+    ]
+
+    const action = {
+        type: 'CHANGE_TODOLIST_TITLE',
+        payload: {
+            id:toDoListId1,
+            title: 'New Todolist',
+        },
+    } as const
+
+    const endState = toDoListsReducer(startState, action)
+
+    expect(endState.length).toBe(2)
+    expect(endState[0].title).toBe(action.payload.title)
+    expect(endState[0].id).toBe(toDoListId1)
+})
+
+test('correct todolist filter should change', () => {
+    const toDoListId1 = v1()
+    const toDoListId2 = v1()
+
+    const startState: ToDoListsType[] = [
+        {id: toDoListId1, title: 'what to learn', filter: 'all'},
+        {id: toDoListId2, title: 'what to buy', filter: 'all'}
+    ]
+
+    const action = {
+        type: 'CHANGE_TODOLIST_FILTER',
+        payload: {
+            id:toDoListId1,
+            filter: 'active',
+        },
+    } as const
+
+    const endState = toDoListsReducer(startState, action)
+
+    expect(endState.length).toBe(2)
+    expect(endState[0].filter).toBe(action.payload.filter)
+    expect(endState[0].id).toBe(toDoListId1)
 })
--- src/model/toDoListsReducer.ts
+++ src/model/toDoListsReducer.ts
@@ -1,14 +1,25 @@
+import {v1} from 'uuid';
 import {ToDoListsType} from '../App';
 
-type ActionType={
-    type:string
-    [key:string]:any
+type ActionType = {
+    type: string
+    payload: any
 }
 
-export const toDoListsReducer =(state:ToDoListsType[],action:ActionType)=>{
-    switch (action.type){
-        case 'REMOVE_TODOLIST':{
-            return state.filter(td => td.id !== id)
+export const toDoListsReducer = (state: ToDoListsType[], action: ActionType) => {
+    switch (action.type) {
+        case 'REMOVE_TODOLIST': {
+            return state.filter(td => td.id !== action.payload.id)
+        }
+        case 'ADD_TODOLIST': {
+            //setTasks({...tasksObj, [id]: []})
+            return [{id: v1(), title: action.payload.title, filter: 'all'}, ...state]
+        }
+        case 'CHANGE_TODOLIST_TITLE': {
+            return state.map(el => el.id === action.payload.id ? {...el, title: action.payload.title} : el)
+        }
+        case 'CHANGE_TODOLIST_FILTER': {
+            return state.map(el => el.id === action.payload.id ? {...el, filter: action.payload.filter} : el)
         }
         default:
             throw new Error('Can\'t find this action type')
