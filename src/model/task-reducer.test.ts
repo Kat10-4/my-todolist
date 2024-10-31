6abd43eda30c36a601142bf6575978808bea7b60
Kat10-4 <143129834+Kat10-4@users.noreply.github.com>
2024-10-31 20:40:22 +0200

    added addTaskAC

--- src/model/task-reducer.test.ts
+++ src/model/task-reducer.test.ts
@@ -1,4 +1,4 @@
-import {removeTaskAC, taskReducer} from './task-reducer'
+import {addTaskAC, removeTaskAC, taskReducer} from './task-reducer'
 import {TasksType} from '../App';
 
 
@@ -31,3 +31,25 @@ test('correct task should be deleted from correct array', () => {
     })
 })
 
+test('correct task should be added to correct array', () => {
+    const startState: TasksType = {
+        todolistId1: [
+            { id: '1', title: 'CSS', isDone: false },
+            { id: '2', title: 'JS', isDone: true },
+            { id: '3', title: 'React', isDone: false },
+        ],
+        todolistId2: [
+            { id: '1', title: 'bread', isDone: false },
+            { id: '2', title: 'milk', isDone: true },
+            { id: '3', title: 'tea', isDone: false },
+        ],
+    }
+
+    const endState = taskReducer(startState, addTaskAC({ newTitle: 'juice', todolistId: 'todolistId2' }))
+
+    expect(endState['todolistId1'].length).toBe(3)
+    expect(endState['todolistId2'].length).toBe(4)
+    expect(endState['todolistId2'][0].id).toBeDefined()
+    expect(endState['todolistId2'][0].title).toBe('juice')
+    expect(endState['todolistId2'][0].isDone).toBe(false)
+})
--- src/model/task-reducer.ts
+++ src/model/task-reducer.ts
@@ -1,26 +1,42 @@
 import {v1} from 'uuid';
-import {FilterValuesType, TasksType, ToDoListsType} from '../App';
+import {FilterValuesType, TasksType, TaskType, ToDoListsType} from '../App';
 
 
 export type RemoveTaskActionType = ReturnType<typeof removeTaskAC>
+export type AddTaskActionType = ReturnType<typeof addTaskAC>
 
-type ActionsType = RemoveTaskActionType
+type ActionsType = RemoveTaskActionType | AddTaskActionType
 
 export const taskReducer = (state: TasksType, action: ActionsType): TasksType => {
     switch (action.type) {
         case 'REMOVE_TASK': {
-            return {...state, [action.payload.todolistId]: state[action.payload.todolistId].filter(el => el.id !== action.payload.id)}
+            return {
+                ...state,
+                [action.payload.todolistId]: state[action.payload.todolistId].filter(el => el.id !== action.payload.id)
+            }
         }
-
+        case 'ADD_TASK': {
+            let newTask: TaskType = {id: v1(), title: action.payload.newTitle, isDone: false}
+            return {
+                ...state, [action.payload.todolistId]: [newTask, ...state[action.payload.todolistId]]
+            }
+        }
         default:
             throw new Error('Can\'t find this action type')
     }
 }
 
-export const removeTaskAC = (payload:{id: string,todolistId: string}) => {
+export const removeTaskAC = (payload: { id: string, todolistId: string }) => {
     return {
         type: 'REMOVE_TASK',
         payload
     } as const
 }
 
+
+export const addTaskAC = (payload: { newTitle: string, todolistId: string }) => {
+    return {
+        type: 'ADD_TASK',
+        payload
+    } as const
+}
