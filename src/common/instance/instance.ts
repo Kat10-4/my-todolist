f0a52f6d38c7459aedfc04ff7301995500fe6665
Kat10-4 <143129834+Kat10-4@users.noreply.github.com>
2025-03-28 16:17:51 +0200

    used env variables

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -1,12 +1,9 @@
 import axios from "axios"
 
-const token = "714ecac0-93f5-4e06-92d8-4a78b2b2eb89"
-const apiKey = "5a84ec24-5118-4689-a393-1705bd1d41e8"
-
 export const instance = axios.create({
-  baseURL: "https://social-network.samuraijs.com/api/1.1",
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
-    Authorization: `Bearer ${token}`,
-    "API-KEY": apiKey,
+    Authorization: `Bearer ${process.env.REACT_APP_AUTH_TOKEN}`,
+    "API-KEY": process.env.REACT_APP_API_KEY,
   },
 })
