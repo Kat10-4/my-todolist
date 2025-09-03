9aba453da4fc8f44af7f85bd2174d402642c58ff
Kat10-4 <143129834+Kat10-4@users.noreply.github.com>
2025-09-03 13:26:35 +0300

    changes palette and fonts

--- src/common/theme/Theme.ts
+++ src/common/theme/Theme.ts
@@ -1,12 +1,27 @@
 import { createTheme } from "@mui/material"
-import { orange, purple } from "@mui/material/colors"
+import { common } from "@mui/material/colors"
 
 export const getTheme = (themeMode: string) => {
   return createTheme({
     palette: {
-      primary: purple,
-      secondary: orange,
+      primary: {
+        main: common.black,
+        light: "#333333",
+        dark: "#000000",
+        contrastText: common.white,
+      },
+      secondary: {
+        main: common.white,
+        light: "#ffffff",
+        dark: "#cccccc",
+        contrastText: common.black,
+      },
       mode: themeMode === "light" ? "light" : "dark",
+    },
+    typography: {
+      fontFamily: `"Source Sans 3", "Arial", sans-serif`,
+      h1: { fontFamily: `"Playfair Display SC", serif` },
+      h2: { fontFamily: `"Playfair Display SC", serif` },
     },
   })
 }
