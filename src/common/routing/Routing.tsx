475222e117830677f8d076eae29a92a04e31c461
Kat10-4 <143129834+Kat10-4@users.noreply.github.com>
2025-09-18 17:28:30 +0300

    feat(ui): implement PageNotFound component

--- src/common/routing/Routing.tsx
+++ src/common/routing/Routing.tsx
@@ -1,15 +1,18 @@
 import { Route, Routes } from "react-router"
 import { Main } from "../../app/Main"
 import { Login } from "../../features/auth/ui/Login/Login"
+import { PageNotFound } from "../components"
 
 export const Path = {
-  Main: '/',
-  Login: 'login',
+  Main: "/",
+  Login: "login",
+  NotFound: "*",
 } as const
 
 export const Routing = () => (
   <Routes>
     <Route path={Path.Main} element={<Main />} />
     <Route path={Path.Login} element={<Login />} />
+    <Route path={Path.NotFound} element={<PageNotFound />} />
   </Routes>
 )
